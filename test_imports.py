38100c0a842f44670509f3fc6d4ff4dc3ae91e97
Jason K. Moore <moorepants@gmail.com>
2024-04-06 18:02:56 +0200

    Little better test imports script.

--- test_imports.py
+++ test_imports.py
@@ -4,34 +4,51 @@ import importlib
 def test_imports():
 
     requested_packages = [
+        'CoolProp',  # package name is coolprop
+        'PyDSTool',  # package name: pydstool
         'asciitree',
         'colorful',
         'configparser',
         'control',
-        'CoolProp',  # package name is coolprop
+        'cython',
         'dwf',  # getting a missing so file
         'intervals',
         'more_itertools',
         'nidaqmx',  # package name: nidaqmx-python
+        'pandas',
         'pulp',
         'pydot',
         'pydotplus',
-        'PyDSTool',  # package name: pydstool
         'pygraphviz',
+        'pythreejs',
         'pyvisa',
         'pyvisgraph',
         'salabim',
+        'scikits.odes',
         'shapely',
         'slycot',
         'stopit',
         'sympy',
     ]
 
+    did, didnt = [], []
     for package in requested_packages:
         try:
             importlib.__import__(package)
         except ImportError:
-            print(package, ' failed to import')
+            didnt.append(package)
+        else:
+            did.append(package)
+
+    title = 'Packages that successfully imported'
+    print(title)
+    print('='*len(title))
+    print('\n'.join(did))
+    title = 'Packages that failed to import'
+    print(title)
+    print('='*len(title))
+    print('\n'.join(didnt))
+
 
 if __name__ == "__main__":
     test_imports()
