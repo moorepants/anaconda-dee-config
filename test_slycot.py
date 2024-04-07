ea1c03ee9997663be77022b7357432c90f927cc8
Jason K. Moore <moorepants@gmail.com>
2024-04-07 11:54:22 +0200

    Made test files pytest compatible and removed the old module loader.

--- test_slycot.py
+++ test_slycot.py
@@ -2,14 +2,15 @@ from numpy import array, ones
 from numpy.testing import assert_allclose
 import slycot
 
-def sb02md_example():
-    A = array([ [0, 1],
-                [0, 0]])
-    Q = array([ [1, 0],
-                [0, 2]])
-    G = array([ [0, 0],
-                [0, 1]])
-    out = slycot.sb02md(2,A,G,Q,'C')
+
+def test_sb02md_example():
+    A = array([[0, 1],
+               [0, 0]])
+    Q = array([[1, 0],
+               [0, 2]])
+    G = array([[0, 0],
+               [0, 1]])
+    out = slycot.sb02md(2, A, G, Q, 'C')
     print('--- Example for sb02md ---')
     print('The solution X is')
     print(out[0])
@@ -17,4 +18,6 @@ def sb02md_example():
     assert_allclose([[2., 1.], [1., 2.]], out[0])
     assert_allclose(0.30901699437494745, out[1])
 
-sb02md_example()
+
+if __name__ == "__main__":
+    test_sb02md_example()
