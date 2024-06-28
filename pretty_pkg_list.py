487b6cef6e449229492cf1bb11895b723c1859b5
Jason K. Moore <moorepants@gmail.com>
2024-06-28 13:59:48 +0200

    Added script to create a locked construct.yaml.

--- pretty_pkg_list.py
+++ pretty_pkg_list.py
@@ -1,11 +1,11 @@
 """
-prints the pkg-list.base.txt constructor output a bit prettier
+Pretty pring the pkg-list.base.txt constructor output and generates a locked
+construct.yaml file for the minimal package list based on pkg-list.base.txt.
 
 """
 
 import re
 import sys
-import pprint
 from ruamel.yaml import YAML
 from ruamel.yaml.comments import CommentedSeq
 
@@ -16,51 +16,47 @@ yaml = YAML()
 with open('construct.yaml', 'r') as f:
     construct_data = yaml.load(f)
 
-#for pkg_txt in sorted(pkgs_raw):
-    #print('-'.join(pkg_txt.split('-')[:-1]))
+msg = "All packages and versions installed:"
+print(msg)
+print('='*len(msg))
+for pkg_txt in sorted(pkgs_raw):
+    print('-'.join(pkg_txt.split('-')[:-1]))
+
+unpinned_specs = construct_data['specs'].copy()
 
 pkg_versions = {}
-new_specs = []
-for required_pkg in sorted(construct_data['specs'], key=lambda x: len(x),
-                           reverse=True):
-    if '::' in required_pkg:
-        required_pkg = required_pkg.split('::')[1]
-    pkg_name = required_pkg.split('#')[0].strip()
+for pkg_name in unpinned_specs:
+    if '::' in pkg_name:
+        pkg_name = pkg_name.split('::')[1]
     for pkg_txt_line in pkgs_raw:
         if re.match(f'^{pkg_name}-\d', pkg_txt_line) is not None:
             pkg_version = pkg_txt_line.split(pkg_name + '-')[1].split('-')[0]
             pkg_versions[pkg_name] = pkg_version
-            new_specs.append('{} =={}'.format(pkg_name, pkg_version))
 
-updated_specs = []
-for pkg in construct_data['specs']:
-    print(pkg)
+locked_specs = []
+for pkg in unpinned_specs:
     if '::' in pkg:
         channel, pkg = pkg.split('::')
     else:
         channel = ''
-    if '#' in pkg:
-        pkg, selector = [s.strip() for s in pkg.split('#')]
-    else:
-        selector = ''
     if pkg in pkg_versions:
         if channel:
             spec_line = '{}::{} =={}'.format(channel, pkg, pkg_versions[pkg])
         else:
             spec_line = '{} =={}'.format(pkg, pkg_versions[pkg])
-        if selector:
-            spec_line = spec_line + '  # {}'.format(selector)
-        updated_specs.append(spec_line)
+        locked_specs.append(spec_line)
     else:
-        updated_specs.append(pkg)
+        locked_specs.append(pkg)
 
-pprint.pprint(sorted(updated_specs))
-
-comments = construct_data['specs']
-construct_data['specs'] = CommentedSeq(updated_specs)
+comments = construct_data['specs'].ca
+construct_data['specs'] = CommentedSeq(locked_specs)
 construct_data['specs']._yaml_comment = comments
 
 yaml.indent(mapping=2, sequence=4, offset=2)
 with open('construct-lock.yaml', 'w') as f:
     yaml.dump(construct_data, f)
-    yaml.dump(construct_data, sys.stdout)
+
+msg = "Locked construct file based on the above installation:"
+print('\n' + msg)
+print('='*len(msg))
+yaml.dump(construct_data, sys.stdout)
